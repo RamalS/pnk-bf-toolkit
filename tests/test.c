2efc074e79ae3f5bc01b369efc0e341f79889b7b
Hurubon <Huruboe@gmail.com>
2020-10-03 15:48:58 +0200

    Fixed errors, refactoring.

--- tests/test.c
+++ tests/test.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-
-#include <stdint.h>
-
-#define pnk_lengthof(x) (sizeof x / sizeof *x)
-#define pnk_range(x) ((struct Range){ x, x + pnk_lengthof(x) - 1})
-#define pnk_to_range(b, e) ((struct Range){ b, e })
-
-#define pnk_range_foreach(T, it, element, range)                             \
-    for (T const* it = range.cbegin; it != range.cend; ++it) {               \
-        T element = *it;
-#define pnk_endforeach }
+#include <pnk/bf_toolkit.h>
 
 int main(int argc, char** argv)
 {
-    uint8_t tape[30000] = {};
-    uint8_t* tape_iterator = tape;
+    unsigned char tape[30000] = {};
+    unsigned char* tape_iterator = tape;
     char const program[] = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
     pnk_bf_interpret(program, &tape_iterator);
     return 0;
